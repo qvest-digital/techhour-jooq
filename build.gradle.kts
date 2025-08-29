8d0c3848ed8add7cd126ffaf02406bfff57c0e71
Michael Clausen <m.clausen@qvest-digital.com>
2025-08-29 12:33:30 +0200

    integrates jooq and flyway into the build process
    * the jooq plugin will spin up a postgres, apply the flyway migrations, let jooq read and convert the definition to java types, which will then be compiled in a later step

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.gradle.internal.impldep.org.junit.platform.launcher.TagFilter.includeTags
 
 plugins {
+    alias (libs.plugins.dev.monosoul.jooq.docker)
     alias (libs.plugins.org.jetbrains.kotlin.jvm)
     alias (libs.plugins.org.asciidoctor.jvm.convert)
 }
@@ -13,12 +14,26 @@ repositories {
 }
 
 dependencies {
+    implementation (libs.org.flywaydb.flyway.core)
+    runtimeOnly    (libs.org.flywaydb.flyway.database.postgresql)
+    implementation (libs.org.jooq.jooq)
+    implementation (libs.org.postgresql.postgresql)
+    jooqCodegen    (libs.org.postgresql.postgresql)
+
     testImplementation (platform (libs.org.junit.junit.bom))
     testImplementation (libs.org.assertj.assertj.core)
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine")
     testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+}
+
+jooq {
+    withContainer {
+        image {
+            name = "postgres:17.5-alpine"
+        }
+    }
 }
 
 kotlin {
@@ -50,9 +65,23 @@ tasks {
         }
     }
 
+    generateJooqClasses {
+        basePackageName.set ("${project.group}.techhour.jooq.internal.jooq")
+        outputDirectory.set (project.layout.buildDirectory.dir ("generated/sources/jooq"))
+        includeFlywayTable.set (false)
+        flywayProperties.apply {
+            put ("flyway.sqlMigrationPrefix", "v")
+            put ("flyway.sqlMigrationSeparator", "-")
+        }
+    }
+
     test {
         useJUnitPlatform {
             excludeTags ("asciidoc")
         }
+    }
+
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     }
 }
