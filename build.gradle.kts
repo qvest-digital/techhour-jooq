6e1f0972696a3955669fc3cb497a3c91bb676468
Michael Clausen <m.clausen@qvest-digital.com>
2025-08-29 11:34:13 +0200

    integrates asciidoc to render content as web-page or slides
    * manual can be pushed to github pages later, using the sources and definitions from this project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.gradle.internal.impldep.org.junit.platform.launcher.TagFilter.includeTags
+
 plugins {
     alias (libs.plugins.org.jetbrains.kotlin.jvm)
+    alias (libs.plugins.org.asciidoctor.jvm.convert)
 }
 
 group   = "com.qvest.digital"
@@ -23,7 +26,33 @@ kotlin {
 }
 
 tasks {
+    val asciidoctortest by registering (Test::class) {
+        group = asciidoctor.get ().group
+
+        useJUnitPlatform {
+            includeTags ("asciidoc")
+        }
+    }
+
+    asciidoctor {
+        dependsOn (asciidoctortest)
+
+        val base = project.layout.projectDirectory.file ("src/main/asciidoc").asFile
+        val output = project.layout.buildDirectory.file ("manual").get ().asFile
+
+        outputDirProperty.set (output)
+        sourceDirProperty.set (base)
+
+        baseDirFollowsSourceDir ()
+
+        sources {
+            include ("index.adoc")
+        }
+    }
+
     test {
-        useJUnitPlatform ()
+        useJUnitPlatform {
+            excludeTags ("asciidoc")
+        }
     }
 }
